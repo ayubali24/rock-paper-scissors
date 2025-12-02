73a55603197df30e0dc8b5c80aea80c3c388b33e
yubie <ayubali2443@gmail.com>
2025-12-02 14:53:05 -0800

    Add getComputerChoice function to randomly return rock, paper, or scissors
    - used Math.random and floor function to generate
    a random number been 0-2, inclusive
    - from there, i check if randnum = 0 then assign
    computer choice the value of "rock", if 1, then
    "paper", if "2", then "scissors"
    - this value is then returned as a string

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// let number;
+// let computerChoice = '';
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0){
+        return "rock"
+    } else if (number === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
