13aac4ecc55942491cbc9e6f785beecfb3f5ce04
yubie <ayubali2443@gmail.com>
2025-12-02 18:05:58 -0800

    Add logic to play entire game
    - created playGame function to simulate playing the
    entire game
    - we moved our playRound function and score vars inside
    our new function
    - we only want to play 5 rounds so we used a for loop
    to achieve that and also moved our humanSelection
    and computerSelection variables within the for loop
    so we are assigned new values every time the loop rounds
    - the game is working as expected and displays the winner
    with the final score in the console along with the 5
    different rounds and outcomes

--- script.js
+++ script.js
@@ -3,8 +3,50 @@
 // let number;
 // let computerChoice = '';
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        const humanChoiceLowerCase = humanChoice.toLowerCase();
+
+        if (humanChoiceLowerCase === computerChoice) {
+            return "It's a draw!";
+        } else if (humanChoiceLowerCase === "rock") {
+            if (computerChoice === "paper") {
+                computerScore += 1;
+                return `You lose! ${computerChoice} beats ${humanChoiceLowerCase}!`
+            }
+            humanScore += 1;
+            return `You win! ${humanChoiceLowerCase} beats ${computerChoice}!`
+
+        } else if (humanChoiceLowerCase === 'paper') {
+            if (computerChoice === 'scissors') {
+                computerScore += 1;
+                return `You lose! ${computerChoice} beats ${humanChoiceLowerCase}!`
+            }
+            humanScore += 1;
+            return `You win! ${humanChoiceLowerCase} beats ${computerChoice}!`
+        } else if (humanChoiceLowerCase === 'scissors') {
+            if (computerChoice === 'rock') {
+                computerScore += 1;
+                return `You lose! ${computerChoice} beats ${humanChoiceLowerCase}!`
+            }
+            humanScore += 1;
+            return `You win! ${humanChoiceLowerCase} beats ${computerChoice}!`
+        }
+    }
+
+    for (let i = 1; i <= 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(humanSelection, computerSelection));
+        console.log(`Human score: ${humanScore}, Computer Score: ${computerScore}`);
+    }
+
+    console.log((humanScore > computerScore) ? `You win with a score of: ${humanScore}!!` : `Computer wins with a score of: ${computerScore}!!`)
+}
+
 
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -22,37 +64,4 @@ function getHumanChoice () {
     return humanChoiceInput;
 }
 
-function playRound (humanChoice, computerChoice) {
-    const humanChoiceInsenstive = humanChoice.toLowerCase();
-
-    if (humanChoiceInsenstive === computerChoice) {
-        return "It's a draw!";
-    } else if (humanChoiceInsenstive === "rock") {
-        if (computerChoice === "paper") {
-            computerScore += 1;
-            return `You lose! ${computerChoice} beats ${humanChoiceInsenstive}!`
-        }
-        humanScore += 1;
-        return `You win! ${humanChoiceInsenstive} beats ${computerChoice}!`
-        
-    } else if (humanChoiceInsenstive === 'paper') {
-        if (computerChoice === 'scissors') {
-            computerScore += 1;
-            return `You lose! ${computerChoice} beats ${humanChoiceInsenstive}!`
-        }
-        humanScore += 1;
-        return `You win! ${humanChoiceInsenstive} beats ${computerChoice}!`
-    } else if (humanChoiceInsenstive === 'scissors') {
-        if (computerChoice === 'rock') {
-            computerScore += 1;
-            return `You lose! ${computerChoice} beats ${humanChoiceInsenstive}!`
-        }
-        humanScore += 1;
-        return `You win! ${humanChoiceInsenstive} beats ${computerChoice}!`
-    }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
+playGame();
