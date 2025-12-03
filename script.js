18fe8d5a41c24c597d5b5277e9cdb10f80065081
yubie <ayubali2443@gmail.com>
2025-12-02 16:27:31 -0800

    Add playRound function to simulate one game round
    - created a playRound function that writes the logic
    for a single round which will take the human and computer
    choices as parameters and announce a winner and increase
    the respective score based on those inputs
    - the function handles case sensitivity for the humanchoice
    input by converting it to lower case

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0){
         return "rock"
-    } else if (number === 1) {
+    } else if (randomNumber === 1) {
         return "paper"
     } else {
         return "scissors"
@@ -18,9 +18,41 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-    let humanChoice = prompt("Choose; rock, paper, or scissors");
-    return humanChoice;
+    let humanChoiceInput = prompt("Choose; rock, paper, or scissors");
+    return humanChoiceInput;
 }
 
+function playRound (humanChoice, computerChoice) {
+    const humanChoiceInsenstive = humanChoice.toLowerCase();
 
+    if (humanChoiceInsenstive === computerChoice) {
+        return "It's a draw!";
+    } else if (humanChoiceInsenstive === "rock") {
+        if (computerChoice === "paper") {
+            computerScore += 1;
+            return `You lose! ${computerChoice} beats ${humanChoiceInsenstive}!`
+        }
+        humanScore += 1;
+        return `You win! ${humanChoiceInsenstive} beats ${computerChoice}!`
+        
+    } else if (humanChoiceInsenstive === 'paper') {
+        if (computerChoice === 'scissors') {
+            computerScore += 1;
+            return `You lose! ${computerChoice} beats ${humanChoiceInsenstive}!`
+        }
+        humanScore += 1;
+        return `You win! ${humanChoiceInsenstive} beats ${computerChoice}!`
+    } else if (humanChoiceInsenstive === 'scissors') {
+        if (computerChoice === 'rock') {
+            computerScore += 1;
+            return `You lose! ${computerChoice} beats ${humanChoiceInsenstive}!`
+        }
+        humanScore += 1;
+        return `You win! ${humanChoiceInsenstive} beats ${computerChoice}!`
+    }
+}
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
